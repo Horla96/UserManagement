867cfb0fa8f56fdfb2640190480fdd831e42ad90
Horla96 <sexysulai@gmail.com>
2026-01-18 19:53:33 +0100

    Implement IUserRepository interface and UserRepository

---
+++ UserManagement API/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using UserManagement_API.Models;
+
+namespace UserManagement_API.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User> GetByIdAsync(int id);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+    }
+}
---
+++ UserManagement API/Repositories/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement_API.Data;
+using UserManagement_API.Interfaces;
+using UserManagement_API.Models;
+
+namespace UserManagement_API.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+           return await _context.Users.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
