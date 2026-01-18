0068f55c9f40c7e1dd7446c6aaa4c8ac55ae1c4a
Horla96 <sexysulai@gmail.com>
2026-01-18 17:45:21 +0100

    Add User to Models folder

---
+++ UserManagement API/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UserManagement_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
